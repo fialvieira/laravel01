49a2472e985d6e6eebd24f7bc2e51b7f8b9794ec
fialvieira <fialvieira@gmail.com>
2025-09-02 13:54:17 -0300

    Controllers especiais.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/init', [MainController::class, 'initMethod'])->name('init');
 Route::get('/view', [MainController::class, 'viewPage'])->name('view');
+
+// Route para controller single action
+Route::get('/single-action', SingleActionController::class)->name('single');
+
+// Route para controller do tipo resource
+// Route::resource('users', UserController::class);
+
+Route::resources([
+    'users' => UserController::class,
+    'clients' => ClientsController::class,
+    'products' => ProductsController::class
+]);
