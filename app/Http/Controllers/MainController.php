0830780bcd6f61180f5d7ab09e8a536b3384a276
fialvieira <fialvieira@gmail.com>
2025-09-02 12:40:30 -0300

    Controller padrão.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,53 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
-    public function index(Request $request)
+    public function initMethod(): string
     {
-        return 'Hello, this is the index method!';
+        return 'This is the init method of MainController';
     }
 
-    public function about(Request $request)
+    public function viewPage(): View
     {
-        return 'Hello, this is the about method!';
+        return view('home');
     }
-
-    public function mostrarValor($valor)
-    {
-        return "O valor é: " . $valor;
-    }
-
-    public function mostrarValores($valor1, $valor2)
-    {
-        return "Os valores são: " . $valor1 . " e " . $valor2;
-    }
-
-    public function mostrarValores2(Request $request, $valor1, $valor2)
-    {
-        return "Os valores são: " . $valor1 . " e " . $valor2;
-    }
-
-    public function mostrarValorOpcional($valor = null)
-    {
-        if ($valor) {
-            return "O valor é: " . $valor;
-        }
-        return "Nenhum valor foi fornecido.";
-    }
-
-    public function mostrarValorOpcional2($valor1, $valor2 = null)
-    {
-        if ($valor2) {
-            return "Os valores são: " . $valor1 . " e " . $valor2;
-        }
-        return "O valor é: " . $valor1 . ". Nenhum segundo valor foi fornecido.";
-    }
-
-    public function showPosts($user_id, $post_id)
-    {
-        return "User ID: " . $user_id . ", Post ID: " . $post_id;
-    }
-
 }
